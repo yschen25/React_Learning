5f4e3f60d38ae49acd230569a2186403c97d0e63
Ka <yschen25@gmail.com>
2019-11-25 23:43:57 +0800

    react

---
+++ React_Day3.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let element = <h1>React Day3</h1>;
+
+ReactDOM.render(element, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+//引用path模組
+const path = require('path');
+module.exports = {
+     // 如果有一個以上的檔案需要打包，可以傳陣列給entry
+    entry: {
+        index: ['./React_Day4.jsx']
+    },
+    output: {
+        // 這裡是打包後的檔案名稱
+        filename: 'bundle2.js',
+        // 打包後的路徑，這裡使用path模組的resolve()取得絕對位置，也就是目前專案的根目錄
+        path: path.resolve('./'),
+    },
+     // 將loader的設定寫在module的rules屬性中
+    module: {
+        // rules的值是一個陣列可以存放多個loader物件
+        rules: [
+            { test: /.js$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-react', '@babel/preset-env'] } } }
+        ]
+    }
+};
